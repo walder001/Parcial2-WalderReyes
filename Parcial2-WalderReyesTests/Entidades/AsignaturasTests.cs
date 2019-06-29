72c57ccc81daf787fd91f99b93906ab46b56d9ff
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-06-29 01:39:23 -0400

    Creacion del tests project.

---
+++ Parcial2-WalderReyesTests/Entidades/AsignaturasTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_WaldeReyes.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_WaldeReyes.Entidades.Tests
+{
+    [TestClass()]
+    public class AsignaturasTests
+    {
+        [TestMethod()]
+        public void AsignaturasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AsignaturasTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
