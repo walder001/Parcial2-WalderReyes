21a6b393a82555d6f2bed03b04568a689c40b8a5
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-06-30 23:23:03 -0400

    Metodos de la consulta terminado.

--- Parcial2-WaldeReyes/UI/Consulta/cMaterias.cs
+++ Parcial2-WaldeReyes/UI/Consulta/cMaterias.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Parcial2_WaldeReyes.DAL;
+using Parcial2_WaldeReyes.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,8 +21,36 @@ namespace Parcial2_WaldeReyes.UI.Consulta
 
         private void BtnBuscar_Click(object sender, EventArgs e)
         {
-            int id = Convert.ToInt32(txtCriterio.Text);
-            listar = BLL.GetList(i => i.InscripcionId == id);
+            var listar = new List<Asignaturas>();
+            BLL.RepositorioBase<Asignaturas> BLL = new BLL.RepositorioBase<Asignaturas>(new Contexto());
+            if (txtCriterio.Text.Trim().Length > 0)
+            {
+                switch (cbFiltro.SelectedItem)
+                {
+                    case 0:
+                        BLL.GetList(i => true);
+                        break;
+                    case 1:
+                        int id = Convert.ToInt32(txtCriterio.Text);
+                        listar = BLL.GetList(i => i.AsignaturaId == id);
+                        break;
+
+                    case 2:
+                        int es = Convert.ToInt32(txtCriterio.Text);
+                        listar = BLL.GetList(i => i.Descripcion.Contains(txtCriterio.Text)); break;
+                    case 3://Descripcion
+                        double mo = Convert.ToDouble(txtCriterio.Text);
+                        listar = BLL.GetList(i => i.Creditos == mo); break;
+
+                }
+
+            }
+            else
+            {
+                listar = BLL.GetList(i => true);
+            }
+            dgvConsulta.DataSource = null;
+            dgvConsulta.DataSource = listar;
         }
 
         private void DgvConsulta_CellContentClick(object sender, DataGridViewCellEventArgs e)
