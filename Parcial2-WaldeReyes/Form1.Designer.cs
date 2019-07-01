ec0a959b5740d2a2f42d69aae747231a58c53ce8
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-01 00:12:14 -0400

    Colocacion de imagen en el mainfrom.

--- Parcial2-WaldeReyes/Form1.Designer.cs
+++ Parcial2-WaldeReyes/Form1.Designer.cs
@@ -28,17 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.registroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.estudianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.asignaturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.inscripcionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.consultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estudianteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignaturaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.inscripcionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Orange;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.registroToolStripMenuItem,
             this.consultaToolStripMenuItem});
@@ -61,41 +68,80 @@
             // estudianteToolStripMenuItem
             // 
             this.estudianteToolStripMenuItem.Name = "estudianteToolStripMenuItem";
-            this.estudianteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.estudianteToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.estudianteToolStripMenuItem.Text = "Estudiante";
             this.estudianteToolStripMenuItem.Click += new System.EventHandler(this.EstudianteToolStripMenuItem_Click);
             // 
             // asignaturaToolStripMenuItem
             // 
             this.asignaturaToolStripMenuItem.Name = "asignaturaToolStripMenuItem";
-            this.asignaturaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.asignaturaToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.asignaturaToolStripMenuItem.Text = "Asignatura";
             this.asignaturaToolStripMenuItem.Click += new System.EventHandler(this.AsignaturaToolStripMenuItem_Click);
             // 
             // inscripcionToolStripMenuItem
             // 
             this.inscripcionToolStripMenuItem.Name = "inscripcionToolStripMenuItem";
-            this.inscripcionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inscripcionToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.inscripcionToolStripMenuItem.Text = "Inscripcion";
             this.inscripcionToolStripMenuItem.Click += new System.EventHandler(this.InscripcionToolStripMenuItem_Click);
             // 
             // consultaToolStripMenuItem
             // 
+            this.consultaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.estudianteToolStripMenuItem1,
+            this.asignaturaToolStripMenuItem1,
+            this.inscripcionToolStripMenuItem1});
             this.consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
             this.consultaToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.consultaToolStripMenuItem.Text = "Consulta";
+            // 
+            // estudianteToolStripMenuItem1
+            // 
+            this.estudianteToolStripMenuItem1.Name = "estudianteToolStripMenuItem1";
+            this.estudianteToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.estudianteToolStripMenuItem1.Text = "Estudiante";
+            this.estudianteToolStripMenuItem1.Click += new System.EventHandler(this.EstudianteToolStripMenuItem1_Click);
+            // 
+            // asignaturaToolStripMenuItem1
+            // 
+            this.asignaturaToolStripMenuItem1.Name = "asignaturaToolStripMenuItem1";
+            this.asignaturaToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.asignaturaToolStripMenuItem1.Text = "Asignatura";
+            this.asignaturaToolStripMenuItem1.Click += new System.EventHandler(this.AsignaturaToolStripMenuItem1_Click);
+            // 
+            // inscripcionToolStripMenuItem1
+            // 
+            this.inscripcionToolStripMenuItem1.Name = "inscripcionToolStripMenuItem1";
+            this.inscripcionToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.inscripcionToolStripMenuItem1.Text = "Inscripcion";
+            this.inscripcionToolStripMenuItem1.Click += new System.EventHandler(this.InscripcionToolStripMenuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(30, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(681, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 440);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.White;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,6 +155,10 @@
         private System.Windows.Forms.ToolStripMenuItem asignaturaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem inscripcionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem consultaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estudianteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem asignaturaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem inscripcionToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Parcial2-WaldeReyes/Form1.cs
+++ Parcial2-WaldeReyes/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Parcial2_WaldeReyes.UI.Registro;
+﻿using Parcial2_WaldeReyes.UI.Consulta;
+using Parcial2_WaldeReyes.UI.Registro;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -38,6 +39,26 @@ namespace Parcial2_WaldeReyes
             rInscripcion asi = new rInscripcion();
             asi.Show();
 
+        }
+
+        private void EstudianteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            cEstudiante asi = new cEstudiante();
+            asi.Show();
+
+        }
+
+        private void AsignaturaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            cMaterias asi = new cMaterias();
+            asi.Show();
+
+        }
+
+        private void InscripcionToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            cInscripcion asi = new cInscripcion();
+            asi.Show();
         }
     }
 }
